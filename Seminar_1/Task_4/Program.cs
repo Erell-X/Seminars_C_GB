b43f8064f98b3c96fcb1dd72d41dd9f0bd3097a7
*Elizarov <Super.elizarov1451@mail.ru>
2023-04-30 18:05:41 +0300

    Добавлены программы к задача №№1-5Семинара 1

---
+++ Seminar_1/Task_4/Program.cs
@@ -0,0 +1,122 @@
+﻿// При введении числа нужно получить день недели 
+
+Console.Clear();
+
+// int number;
+
+// Console.WriteLine("Введите число");
+// number = Convert.ToInt32(Console.ReadLine());
+
+// if (number<1 | number>7)
+
+// {
+//   Console.WriteLine("Измените число");
+// }
+
+// if (number == 1)
+
+// {
+//   Console.WriteLine("Понедельник");
+// }
+
+// else if (number == 2)
+
+// {
+//   Console.WriteLine("Вторник");
+// }
+
+// else if (number == 3)
+
+// {
+//   Console.WriteLine("Среда");
+// }
+
+// else if (number == 4)
+
+// {
+//   Console.WriteLine("Четверг");
+// }
+
+// else if (number == 5)
+
+// {
+//   Console.WriteLine("Пятница");
+// }
+
+// else if (number == 6)
+
+// {
+//   Console.WriteLine("Суббота");
+// }
+
+// else if (number == 7)
+
+// {
+//   Console.WriteLine("Воскресенье");
+// }
+
+int day = 0;
+
+Console.WriteLine("Введите день недели");
+
+day = Convert.ToInt32(Console.ReadLine());
+
+switch(day)
+{
+    case 1:
+      {
+          Console.WriteLine("Понедельник");
+          Console.WriteLine();
+          break;
+      }
+
+    case 2:
+      {
+          Console.WriteLine("Вторник");
+          Console.WriteLine();
+          break;
+      }
+
+    case 3:
+      {
+          Console.WriteLine("Среда");
+          Console.WriteLine();
+          break;
+      }
+
+    case 4:
+      {
+          Console.WriteLine("Четверг");
+          Console.WriteLine();
+          break;
+      }
+
+    case 5:
+      {
+          Console.WriteLine("Пятница");
+          Console.WriteLine();
+          break;
+      }
+
+    case 6:
+      {
+          Console.WriteLine("Суббота");
+          Console.WriteLine();
+          break;
+      }
+
+    case 7:
+      {
+          Console.WriteLine("Воскресенье");
+          Console.WriteLine();
+          break;
+      }
+    
+    default:
+      {
+          Console.WriteLine("Нет такого дня недели");
+          Console.WriteLine();
+          break;
+      }
+
+}
---
+++ Seminar_1/Task_5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа. 
+// 456 -> 6 
+// 782 -> 2 
+// 918 -> 8
+
+int Number, aNumber, LastFigure;
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число");
+
+Number = Convert.ToInt32(Console.ReadLine());
+aNumber = Number;
+
+if (Number<0)
+{
+  Number=-aNumber;
+}
+
+if (Number>99 && Number<1000)
+{
+  LastFigure = Number % 10;
+  Console.WriteLine($"Последняя цифра числа {aNumber} равна {LastFigure}");
+}
+
+else
+{
+  Console.WriteLine($"Число {aNumber} не является трёхзначным");
+}
