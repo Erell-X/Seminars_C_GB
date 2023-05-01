946b11fd966b30022661bf4ee8b1aaee5143560c
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-01 21:39:49 +0300

    Решение задач с семинаров №№2-3

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -6,3 +6,25 @@
 // 12-> 2
 // 85 -> 8
 
+Console.Clear();
+
+int Num;
+
+Num = new Random().Next(10,100);
+
+Console.WriteLine(Num);
+
+if (Num / 10 > Num % 10)
+{
+  Console.WriteLine(Num / 10);
+}
+
+else if (Num / 10 < Num % 10)
+{
+  Console.WriteLine(Num % 10);
+}
+
+else if (Num / 10 == Num % 10)
+{
+  Console.WriteLine(Num % 10);
+}
---
+++ Seminar_3/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Решение задачи из семинара №2 другим способом.
+
+// Напишите программу, которая выводит случайное трехзначное число и 
+// удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int NewNumber(int N)         // Функциональная зависимость N от NewNumber
+{
+  int digit1 = N / 100;
+  int digit3 = N % 10;
+  int result = digit1 * 10 + digit3;
+
+  return result;
+}
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"Случайное число: {number}");
+int result = NewNumber(number);                     // На основе модели метода вводим аналогичный алгоритм, но с другой переменной.
+
+Console.WriteLine(result);
