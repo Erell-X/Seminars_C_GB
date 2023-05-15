8479833d52bede965adb927764d14955ef4ba5dd
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-15 21:34:10 +0300

    Добавлена программа к задаче №2 3-го семинара по C#

---
+++ Seminar_3/Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты
+// точки (X и Y), причём X≠0 и Y≠0 и выдаёт номер четверти плоскости, в которой
+// находится эта точка. 
+
+void QuatersFind (int x, int y)
+{
+  if (x>0 && y>0)
+  {
+    Console.Write("Точка лежит в I (первой) четверти");
+  }
+  else if (x<0 && y>0)
+  {
+    Console.Write("Точка лежит во II (второй) четверти");
+  }
+  else if (x<0 && y<0)
+  {
+    Console.Write("Точка лежит в III (третьей) четверти");
+  }
+  else if (x>0 && y<0)
+  {
+    Console.Write("Точка лежит в IV (четвёртой) четверти");
+  }
+
+}
+
+Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+QuatersFind(x, y);
