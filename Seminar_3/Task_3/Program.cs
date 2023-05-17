03298863288489a716519f37b032e4553474a39f
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-17 20:03:33 +0300

    Программа к задаче №3 Семинара3 по C#

--- Seminar_3/Task_3/Program.cs
+++ Seminar_3/Task_3/Program.cs
@@ -1,52 +1,58 @@
 ﻿// Напишите программу, которая по заданному номеру четверти показывает 
 // диапазон возможных координат точек в этой четверти (x и y).
 
-string Quater (int N)
-{
-  string range = " ";
-  if (N==1)
-  {
-    range = "x>0, y>0";
-  }
-  else if (N==2)
-  {
-    range = "x<0, y>0";
-  }
-  else if (N==3)
-  {
-    range = "x<0, y<0";
-  }
-  else if (N==4)
-  {
-    range = "x>0, y<0";
-  }
-  else
-  {
-    System.Console.WriteLine("Такой четверти не существует");
-  }
-  return range;
-}
-
-Console.Write("Введите номер четверти: ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-string QuaterFind = Quater(N);
-Console.WriteLine(QuaterFind);
-
-// string[] quaters = {
-//   "x>0, y>0",
-//   "x<0, y>0",
-//   "x<0, y<0",
-//   "x>0, y<0",
-// };
+// string Quater (int N)
+// {
+//   string range = " ";
+//   if (N==1)
+//   {
+//     range = "x>0, y>0";
+//   }
+//   else if (N==2)
+//   {
+//     range = "x<0, y>0";
+//   }
+//   else if (N==3)
+//   {
+//     range = "x<0, y<0";
+//   }
+//   else if (N==4)
+//   {
+//     range = "x>0, y<0";
+//   }
+//   else
+//   {
+//     System.Console.WriteLine("Такой четверти не существует");
+//   }
+//   return range;
+// }
 
 // Console.Write("Введите номер четверти: ");
-// int quater = Convert.ToInt32(Console.ReadLine());
-// if (quater >= 1 && quater <= 4)
-// {
-//   Console.WriteLine(quaters[quater-1]);
-// }
-// else
-// {
-//   System.Console.WriteLine("Неправильный номер четверти");
-// }
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// string QuaterFind = Quater(N);
+// Console.WriteLine(QuaterFind);
+
+string FindDiapason(string[] quaters, int quater)
+{
+  string result = quaters[quater-1];
+  return result;
+}
+
+string[] quaters = {
+  "x>0, y>0",
+  "x<0, y>0",
+  "x<0, y<0",
+  "x>0, y<0",
+};
+
+Console.Write("Введите номер четверти: ");
+int quater = Convert.ToInt32(Console.ReadLine());
+if (quater >= 1 && quater <= 4)
+{
+  Console.WriteLine(FindDiapason(quaters, quater));
+}
+else
+{
+  System.Console.WriteLine("Неправильный номер четверти");
+}
