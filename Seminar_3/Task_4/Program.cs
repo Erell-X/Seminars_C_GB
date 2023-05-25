fbbb013d69fdbcb50c0781a8c777c63e2b1d80a4
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-25 21:36:31 +0300

    Добавление задачи 4 Семинара3 по c#

--- Seminar_3/Task_4/Program.cs
+++ Seminar_3/Task_4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+// квадратов чисел от 1 до N
+
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// int square = 1;
+// while (num >= square)
+// {
+//   double Mult = Math.Pow(square, 2);
+//   square++;
+//   System.Console.Write($"{Mult} ");
+// }
+
+for (int i = 1; i <= num; i++)
+{
+  double Lin = Math.Pow(i, 2);
+  System.Console.Write($"{Lin} ");
+}
