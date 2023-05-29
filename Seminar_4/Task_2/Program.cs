2d41b61bdbdf582c85de341292c8f540fbd1dd70
*Elizarov <Super.elizarov1451@mail.ru>
2023-05-29 21:00:02 +0300

    Программа к Задаче-2 Семинара-4 по C#

---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и
+// выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// System.Console.WriteLine("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int Sum = 0;
+
+// for (int i = 1; i <= A; i++)
+// {
+//   Sum += i;
+// }
+
+// System.Console.WriteLine(Sum);
+
+int Sum(int A)
+{
+  int sum = 0;
+  for (int i = 1; i <= A; i++)
+  {
+    sum += i;
+  }
+  return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
